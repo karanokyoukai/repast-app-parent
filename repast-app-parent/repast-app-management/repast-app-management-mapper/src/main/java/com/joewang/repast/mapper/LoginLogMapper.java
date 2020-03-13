281e0c6024e20f1372b8a6da6092b25b2f8efc22
shadows_s <karanokyoukai@users.noreply.github.com>
2020-03-13 14:16:57 +0800

    删除了第二级的repast-app-parent

--- repast-app-parent/repast-app-management/repast-app-management-mapper/src/main/java/com/joewang/repast/mapper/LoginLogMapper.java
+++
@@ -1,9 +0,0 @@
-package com.joewang.repast.mapper;
-
-import com.joewang.repast.model.LoginLog;
-import org.springframework.stereotype.Repository;
-import tk.mybatis.mapper.common.Mapper;
-
-@Repository
-public interface LoginLogMapper extends Mapper<LoginLog> {
-}
