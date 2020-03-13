cdb1312aa3427c38e377e23532bf8b655d0c2231
shadows_s <karanokyoukai@users.noreply.github.com>
2020-03-13 13:31:26 +0800

    3-12日架构基础

--- repast-app-common/src/main/java/com/joewang/repast/staticstatus/StaticCode.java
+++ repast-app-common/src/main/java/com/joewang/repast/staticstatus/StaticCode.java
@@ -9,13 +9,57 @@ package com.joewang.repast.staticstatus;
  **/
 public class StaticCode {
 
+    /**
+     * 常量字符集
+     */
     public static final String ENCODING = "UTF-8";
+    /**
+     * 常量省份
+     */
     public static final String PROVINCE = "province";
+    /**
+     * 常量国家
+     */
     public static final String COUNTRY = "country";
+    /**
+     * 常量市区
+     */
     public static final String CITY = "city";
+    /**
+     * 常量时间格式
+     */
     public static final String FORMAT_DATE = "yyyy-MM-dd HH:mm:ss";
     // TODO 这里的ip是测试数据，上线的时候注意替换
     public static final String TEST_IP = "222.137.210.39";
+    /**
+     * 常量nx
+     */
+    public static final String NX = "nx";
+
+    /**
+     * 常量xx
+     */
+    public static final String XX = "xx";
+
+    /**
+     * 常量ex
+     */
+    public static final String EX = "ex";
+
+    /**
+     * 常量px
+     */
+    public static final String PX = "px";
+
+    /**
+     * 常量ok
+     */
+    public static final String OK = "OK";
+
+    /**
+     * 常量no
+     */
+    public static final String NO = "NO";
 
 
 }
