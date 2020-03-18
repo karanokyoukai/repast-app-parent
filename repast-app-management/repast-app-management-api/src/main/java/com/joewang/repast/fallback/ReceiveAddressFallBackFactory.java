f86679355a7eb69b3ae67356e56587fcc1ae165f
Steven1124 <59271227+Steven1124@users.noreply.github.com>
2020-03-18 15:24:33 +0800

    Ftp上传接口
    修改一期项目中的熔断方法，增加ftp工具接口

---
+++ repast-app-management/repast-app-management-api/src/main/java/com/joewang/repast/fallback/ReceiveAddressFallBackFactory.java
@@ -0,0 +1,47 @@
+/**
+ * @Company AAA软件教育
+ * @Author Zero
+ * @Date Create in 2020/3/18 14:39
+ * @Description
+ **/
+package com.joewang.repast.fallback;
+
+import com.joewang.repast.model.ReceiveAddress;
+import com.joewang.repast.service.IReceiveAddress;
+import feign.hystrix.FallbackFactory;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+@Component
+public class ReceiveAddressFallBackFactory implements FallbackFactory<IReceiveAddress> {
+    @Override
+    public IReceiveAddress create(Throwable throwable) {
+        IReceiveAddress receiveAddress=new IReceiveAddress() {
+            @Override
+            public List<ReceiveAddress> selectAllReceiveAddress(long id) {
+                return null;
+            }
+
+            @Override
+            public ReceiveAddress selectByKeyReceiveAddress(long id) {
+                return null;
+            }
+
+            @Override
+            public Boolean updateReceiveAddress(ReceiveAddress receiveAddress) {
+                return null;
+            }
+
+            @Override
+            public Boolean insertReceiveAddress(ReceiveAddress receiveAddress) {
+                return null;
+            }
+
+            @Override
+            public Boolean deleteReceiveAddress(long id) {
+                return null;
+            }
+        };
+        return null;
+    }
+}
